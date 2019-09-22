a032464fb0a12185d84d2e5e925accbc65da1db7
AstmDesign <astm_desig@hotmail.com>
2019-09-22 13:26:15 +0200

    server ip

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -7,7 +7,7 @@
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
 
-server '0.0.0.0', user: 'deploy', roles: %w{app db web}
+server '165.227.235.153', user: 'deploy', roles: %w{app db web}
 
 
 # role-based syntax
